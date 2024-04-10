776d22193ea39e43445193531c0b11ecb8958f7a
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-04-10 21:53:19 +0530

    featUpdate:added search page

---
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>This is search page</div>
+  )
+}
+
+export default page
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,11 +1,16 @@
-import Sidebar from "./_components/Sidebar"
+import Sidebar from "./_components/Sidebar";
 
-const layout = () => {
+const layout = ({ children }: { children: React.ReactNode }) => {
   return (
+    <div className="h-full">
     <div className="hidden md:flex h-full w-56 flex-col fixed inset-y-0 z-50">
-    <Sidebar />
-  </div>
-  )
-}
+      <Sidebar />
+    </div>
+    <main className="ml-56 h-full">
+      {children}
+    </main>
+    </div>
+  );
+};
 
-export default layout
+export default layout;
