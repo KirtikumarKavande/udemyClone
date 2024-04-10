e9fd1d1d4238c211dbd5e0122b5c21a9d018151d
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-04-10 22:18:22 +0530

    style:added navbar in mobile view

---
+++ app/(dashboard)/_components/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MobileSidebar from './MobileSidebar'
+
+const Navbar = () => {
+  return (
+    <div
+    className='p-4 border-b h-full flex items-center bg-white shadow-sm'
+    ><MobileSidebar/></div>
+  )
+}
+
+export default Navbar
