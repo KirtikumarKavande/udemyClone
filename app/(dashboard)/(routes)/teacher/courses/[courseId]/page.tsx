9b32c17da4febc55746f3dd4312aba10cfcf05b9
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-04-18 09:30:50 +0530

    feat:added price component to set the price

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,13 +1,14 @@
 import { IconBadge } from "@/components/icon-badge";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
-import { LayoutDashboard } from "lucide-react";
+import { CircleDollarSign, LayoutDashboard, ListChecks } from "lucide-react";
 import { redirect } from "next/navigation";
 import React from "react";
 import TitleForm from "./_components/title-form";
 import DescriptionForm from "./_components/description-form";
 import ImageForm from "./_components/image-form";
 import CategoryForm from "./_components/category-form";
+import PriceForm from "./_components/price-form";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const { userId } = auth();
@@ -55,14 +56,27 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
           <ImageForm initialData={course} />
           <CategoryForm
             initialData={course}
-            options={
-            
-              categories.map((item) => ({
-                label: item.name,
-                value: item.id,
-              }))
-            }
+            options={categories.map((item) => ({
+              label: item.name,
+              value: item.id,
+            }))}
           />
+        </div>
+        <div className="space-y-6">
+          <div>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={ListChecks} />
+              <h2 className="text-xl">Course Chapters</h2>
+            </div>
+            <div>TODO:Chapters</div>
+          </div>
+          <div>
+            <div className="flex items-center gap-x-2">
+              <IconBadge icon={CircleDollarSign} />
+              <h2 className="text-xl">Sell your Course</h2>
+            </div>
+            <PriceForm initialData={course}/>
+          </div>
         </div>
       </div>
     </div>
