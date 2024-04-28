34a524e92cdaef69154ed071190fc4a9ef3b6ea3
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-04-28 16:09:23 +0530

    feat:added table for showing all the courses added by user

---
+++ app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { Course } from "@prisma/client"
+import { ColumnDef } from "@tanstack/react-table"
+import { ArrowUpDown, MoreHorizontal } from "lucide-react"
+
+export type Payment = {
+  id: string
+  amount: number
+  status: "pending" | "processing" | "success" | "failed"
+  email: string
+}
+
+export const columns: ColumnDef<Course>[] = [
+  {
+    accessorKey: "title",
+    header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+           Title
+            <ArrowUpDown className="ml-2 h-4 w-4" />
+          </Button>
+        )
+      },
+  },
+  {
+    accessorKey: "price",
+    header: "Price",
+  },
+  {
+    accessorKey: "isPublished",
+    header: "Published",
+  },
+]
--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,10 +1,26 @@
 import { Button } from "@/components/ui/button";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 import Link from "next/link";
 import React from "react";
+import { db } from "@/lib/db";
+import { DataTable } from "./_components/data-table";
+import { columns } from "./_components/columns";
 
-const CoursesPage = () => {
+const CoursesPage = async() => {
+  const {userId}=auth()
+
+  if(!userId){
+    return redirect("/")
+  }
+
+  const courses = await db.course.findMany({
+    
+    where: { userId },orderBy: {createdAt: "desc"}
+  })
   return (
     <div className="p-6">
+      <DataTable columns={columns} data={courses}/>
       <Link href="/teacher/create">
         <Button >New Courses</Button>
       </Link>
