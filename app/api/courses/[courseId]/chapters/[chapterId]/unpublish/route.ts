1a91df1a937b2d0cbe98b71bbd89b76bba8af45d
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-04-28 09:04:59 +0530

    feat:chapter unpublished

---
+++ app/api/courses/[courseId]/chapters/[chapterId]/unpublish/route.ts
@@ -0,0 +1,53 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { courseId: string; chapterId: string } }
+) {
+  try {
+    const { userId } = auth();
+    if (!userId) return new NextResponse("unauthorized", { status: 401 });
+
+    const isOwner = await db.course.findUnique({
+      where: {
+        id: params.courseId,
+        userId,
+      },
+    });
+
+    if (!isOwner) return new NextResponse("unauthorized", { status: 401 });
+
+    const unPublishedChapter = await db.chapter.update({
+      where: {
+        id: params.chapterId,
+        courseId: params.courseId,
+      },
+      data: {
+        isPublished: false,
+      },
+    });
+
+    const publishedChapterInCourse = await db.chapter.findMany({
+      where: {
+        courseId: params.courseId,
+        isPublished: true,
+      },
+    });
+
+    if (!publishedChapterInCourse.length) {
+      await db.course.update({
+        where: {
+          id: params.courseId,
+        },
+        data: {
+          isPublished: false,
+        },
+      });
+    }
+    return NextResponse.json(unPublishedChapter);
+  } catch (error) {
+    console.log("[unpublish chapter] error", error);
+  }
+}
