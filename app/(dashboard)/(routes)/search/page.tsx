dfdc35f45f23a2a858ed8e39f7837ebea46f6934
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-04-29 18:50:43 +0530

    style:added search bar mobile menu

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,16 +1,22 @@
 import { db } from "@/lib/db";
 import React from "react";
 import Categories from "./_components/Categories";
+import SearchInput from "@/components/search-input";
 
-const page = async () => {
+const SearchPage = async () => {
   const categories = await db.category.findMany({
     orderBy: { name: "asc" },
   });
-  return <div
-  className="p-6"
-  >
-    <Categories items={categories}/>
-  </div>;
+  return (
+    <>
+    <div className="px-6 pt-6 md:hidden md:mb-0 block">
+      <SearchInput/>
+    </div>
+    <div className="p-6">
+      <Categories items={categories} />
+    </div>
+    </>
+  );
 };
 
-export default page;
+export default SearchPage;
