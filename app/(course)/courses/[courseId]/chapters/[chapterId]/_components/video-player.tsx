52efff6cdf07f3c5ce2c9bf6f5a5494412119a89
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-05-02 15:19:54 +0530

    feat:added video player

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
@@ -1,6 +1,8 @@
 "use client";
+import { cn } from "@/lib/utils";
+import MuxPlayer from "@mux/mux-player-react";
 import { Loader2, Lock } from "lucide-react";
-import React from "react";
+import React, { useState } from "react";
 interface VideoPlayerProps {
   playbackId: string;
   courseId: string;
@@ -19,9 +21,10 @@ const VideoPlayer = ({
   completeOnEnd,
   title,
 }: VideoPlayerProps) => {
+  const [isReady, setIsReady] = useState(false);
   return (
     <div className="relative aspect-video">
-      {!isLocked && (
+      {!isReady && !isLocked && (
         <div className="absolute inset-0 flex items-center justify-center bg-slate-800">
           <Loader2 className="w-8 h-8 animate-spin text-secondary" />
         </div>
@@ -31,6 +34,17 @@ const VideoPlayer = ({
           <Lock className="w-8 h-8" />
           <p>This Chapter is Locked</p>
         </div>
+      )}
+
+      {!isLocked && (
+        <MuxPlayer
+          title="title"
+          className={cn(!isReady && "hidden")}
+          onCanPlay={() => setIsReady(true)}
+          playbackId={playbackId}
+          onEnded={() => {}}
+          autoPlay
+        />
       )}
     </div>
   );
