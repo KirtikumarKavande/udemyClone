4408fdf012b3e9a4ace16fc546ca477ca089377f
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-05-03 15:36:15 +0530

    feat:setup for stripe webhook

---
+++ app/api/webhook/route.ts
@@ -0,0 +1,48 @@
+import Stripe from "stripe";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+import { stripe } from "@/lib/stripe";
+import { db } from "@/lib/db";
+
+export async function POST(req: Request) {
+  const body = await req.text();
+
+  const signature = headers().get("Stripe-Signature") as string;
+
+  let event: Stripe.Event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET!
+    );
+  } catch (error) {
+    return NextResponse.json("webhook error", { status: 400 });
+  }
+
+  const session = event.data.object as Stripe.Checkout.Session;
+
+  const userId = session?.metadata?.userId;
+  const courseId = session?.metadata?.courseId;
+
+  if (event.type === "checkout.session.completed") {
+    if (!userId || !courseId) {
+      return new NextResponse("webhook Error:Missing metadata ", {
+        status: 400,
+      });
+    }
+    await db.purchases.create({
+      data: {
+        courseId,
+        userId,
+      },
+    });
+  }else{
+    return new NextResponse("webhook Error:Unhandled event type ", {
+        status: 200,
+    })
+  }
+
+  return new NextResponse(null, { status: 200 });
+}
