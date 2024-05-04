4441a2762625089ed54e14ba1cde3eb076bf0bb1
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-05-04 10:38:27 +0530

    feat:complete the chapter on clicking on mark completed & revert it

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-progress-button.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/course-progress-button.tsx
@@ -1,7 +1,11 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import axios from "axios";
 import { CheckCircle, XCircle } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
 interface CourseProgressButtonProps {
   chapterId: string;
@@ -15,18 +19,51 @@ const CourseProgressButton = ({
   isCompleted,
   nextChapterId,
 }: CourseProgressButtonProps) => {
-    const Icon=isCompleted?XCircle:CheckCircle
-  return <Button
-  className="w-full md:w-auto"
-  type="button"
-  variant={isCompleted ? "outline" : "success"}
-  >
-    {
-        isCompleted?"Not completed":"Mark as complete"
-        
+  const router = useRouter();
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onClick = async () => {
+    try {
+      await axios.put(
+        `/api/courses/${courseId}/chapters/${chapterId}/progress`,
+        {
+          isCompleted: !isCompleted,
+        }
+      );
+
+      if (!isCompleted && !nextChapterId) {
+        toast.success("Congratulations for Successfully completion of course ");
+      }
+
+      if (!isCompleted && nextChapterId) {
+        router.push(`/courses/${courseId}/chapters/${nextChapterId}`);
+      }
+
+      toast.success("progress Updated Successfully");
+
+      router.refresh()
+    } catch (error) {
+      toast.error("something went wrong");
+    } finally {
+      setIsLoading(false);
     }
-    <Icon className="w-4 h-4 ml-2" />
-  </Button>;
+  };
+
+  const Icon = isCompleted ? XCircle : CheckCircle;
+
+  return (
+    <Button
+    onClick={onClick}
+    disabled={isLoading}
+      className="w-full md:w-auto"
+      type="button"
+      variant={isCompleted ? "outline" : "success"}
+    >
+      {isCompleted ? "Not completed" : "Mark as complete"}
+      <Icon className="w-4 h-4 ml-2" />
+    </Button>
+  );
 };
 
 export default CourseProgressButton;
---
+++ app/api/courses/[courseId]/chapters/[chapterId]/progress/route.ts
@@ -0,0 +1,36 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+export async function PUT(
+  req: Request,
+  { params }: { params: { courseId: string; chapterId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const { isCompleted } = await req.json();
+    if (!userId) return new NextResponse("unauthorized", { status: 401 });
+
+    const userProgress = await db.userProgress.upsert({
+      where: {
+        userId_chapterId: {
+          userId,
+          chapterId: params.chapterId,
+        },
+      },
+      update: {
+        isCompleted,
+      },
+      create: {
+        userId,
+        chapterId: params.chapterId,
+        isCompleted,
+      },
+    });
+
+    return NextResponse.json(userProgress)
+  } catch (error) {
+    console.log("[chapter_ID_PROGRESS", error);
+    return new NextResponse("internal server Error", { status: 500 });
+  }
+}
