c58aa1b767b91532e6b2696d18a7c6f918026218
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-05-04 22:21:57 +0530

    feat:Info card of course completed and progress added

--- app/(dashboard)/(routes)/(root)/page.tsx
+++ app/(dashboard)/(routes)/(root)/page.tsx
@@ -3,6 +3,8 @@ import { redirect } from "next/navigation";
 import Image from "next/image";
 import GetDashboardCourses from "@/actions/get-dashboard-courses";
 import CoursesList from "@/components/courses.list";
+import { CheckCircle, Clock } from "lucide-react";
+import InfoCard from "./_components/info-card";
 
 export default async function Dashboard() {
   const { userId } = auth();
@@ -18,8 +20,17 @@ export default async function Dashboard() {
   return (
     <div className="p-6 space-y-4">
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
-        <div>{/* TODO:INFO CARD */}</div>
-        <div>{/* TODO:INFO CARD */}</div>
+        <InfoCard
+          icon={Clock}
+          label="In progress"
+          numberOfItems={coursesInProgress.length}
+        />
+        <InfoCard
+          icon={CheckCircle}
+          label="completed"
+          numberOfItems={completedCourses.length}
+          variant="success"
+        />
       </div>
 
       <CoursesList items={[...coursesInProgress, ...completedCourses]} />
