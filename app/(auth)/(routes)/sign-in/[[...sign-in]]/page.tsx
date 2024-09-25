4a366af25c4ba863865ed87ded944543851b2916
KirtikumarKavande <kirtikumar0005@gmail.com>
2024-09-25 15:09:01 +0530

    add test credential

--- app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/(routes)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,13 @@
 import { SignIn } from "@clerk/nextjs";
  
 export default function Page() {
-  return <SignIn />;
+  return <div >
+
+    <div className="text-blue-700 text-sm ml-10 mb-4">
+
+    <p >email: yuvrajdhonisachin@gmail.com</p>
+    <p >password: Kirtikumar@1</p>
+    </div>
+    <SignIn />
+  </div> ;
 }
--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div className="h-full flex justify-center items-center">{children}</div>;
+  return <div className="h-full flex justify-center items-center">
+    {children}</div>;
 };
 
 export default AuthLayout;
